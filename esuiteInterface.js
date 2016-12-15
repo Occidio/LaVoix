f9073d1b61b1ac0350fa4f4d511826ed564fb205
Rares Janea <rares.janea@mppglobal.com>
2016-12-15 15:56:59 +0000

    More changes

--- esuiteInterface.js
+++ esuiteInterface.js
@@ -8,7 +8,7 @@ function esuiteInterface() {};
 esuiteInterfacePrototype.ConfigurationMiscCharge = function (callback) {
 
 	// Set the headers
-	let headers = {
+	var headers = {
 		'Content-Type': 'application/json',
 		'X-Version': '1.0.0',
 		'X-ClientId': '433',
@@ -17,11 +17,11 @@ esuiteInterfacePrototype.ConfigurationMiscCharge = function (callback) {
 	};
 
 	// Configure the request
-	let options = {
-		url: 'https://uat.mppglobal.com/api/workflows/configurations/adhoc',
-		method: 'POST',
-		headers: headers,
-		json: {
+	var options = {
+		'url': 'https://uat.mppglobal.com/api/workflows/configurations/adhoc',
+		'method': 'POST',
+		'headers': headers,
+		'json': {
 			"orderItems": [{
 				"description": "Grapegruit purchase",
 				"priceBreakDown": {
@@ -37,20 +37,21 @@ esuiteInterfacePrototype.ConfigurationMiscCharge = function (callback) {
 			}
 		}
 	};
+	callback(false, 'sessionToken');
 
-	request(options, function (error, response, body) {
-		if (error) {
-			callback(true, response);
-			return;
-		}
-		var sessionToken = body.sessionToken;
-		callback(false, sessionToken);
-	})
+	// request(options, function (error, response, body) {
+	// 	if (error) {
+	// 		callback(true, response);
+	// 		return;
+	// 	}
+	// 	var sessionToken = body.sessionToken;
+	// 	callback(false, sessionToken);
+	// })
 };
 
 esuiteInterfacePrototype.AuthenticateAccount = function (session, callback) {
 	// Set the headers
-	let headers = {
+	var headers = {
 		'Content-Type': 'application/json',
 		'X-Version': '1.0.0',
 		'X-TokenId': '9E9F3BEF7D814538AB75AD43CC6D651B',
@@ -59,7 +60,7 @@ esuiteInterfacePrototype.AuthenticateAccount = function (session, callback) {
 	};
 
 	// Configure the request
-	let options = {
+	var options = {
 		url: 'https://uat.mppglobal.com/api/accounts/authenticate',
 		method: 'POST',
 		headers: headers,
@@ -74,15 +75,15 @@ esuiteInterfacePrototype.AuthenticateAccount = function (session, callback) {
 			callback(true, response);
 			return;
 		}
-		let sessionToken = body.sessionToken;
-		let accountId = body.accountId;
+		var sessionToken = body.sessionToken;
+		var accountId = body.accountId;
 		callback(false, sessionToken, accountId);
 	});
 };
 
 esuiteInterfacePrototype.CheckAccountEntitlement = function (session, accountId, callback) {
 	// Set the headers
-	let headers = {
+	var headers = {
 		'Content-Type': 'application/json',
 		'X-Version': '1.0.0',
 		'X-TokenId': '9E9F3BEF7D814538AB75AD43CC6D651B',
@@ -91,7 +92,7 @@ esuiteInterfacePrototype.CheckAccountEntitlement = function (session, accountId,
 	};
 
 	// Configure the request
-	let options = {
+	var options = {
 		url: 'https://uat.mppglobal.com/api/accounts/' + accountId + '/entitlements',
 		method: 'GET',
 		headers: headers
@@ -102,14 +103,14 @@ esuiteInterfacePrototype.CheckAccountEntitlement = function (session, accountId,
 			callback(true, response);
 			return;
 		}
-		let entitlements = body.entitlements;
+		var entitlements = body.entitlements;
 		callback(false, entitlements);
 	});
 };
 
 esuiteInterfacePrototype.ProcessPayment = function (session, callback) {
 	// Set the headers
-	let headers = {
+	var headers = {
 		'Content-Type': 'application/json',
 		'X-Version': '1.0.0',
 		'X-TokenId': '9E9F3BEF7D814538AB75AD43CC6D651B',
@@ -118,7 +119,7 @@ esuiteInterfacePrototype.ProcessPayment = function (session, callback) {
 	};
 
 	// Configure the request
-	let options = {
+	var options = {
 		url: 'https://uat.mppglobal.com/api/workflows/purchases/adhoc',
 		method: 'POST',
 		headers: headers,
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 var EsuiteInterface = require('./esuiteInterface');
 var Alexa = require('alexa-sdk');
+var request = require('request');
+var http = require('http');
 
-this.attributes["sessionToken"];
-this.attributes["currentAccountId"];
-
-var checkEntitlementsFull = function () {
+var checkEntitlementsFull = function (callback) {
     var esuiteInterface = new EsuiteInterface();
     console.log('Starting: Config Miss Charge');
     esuiteInterface.ConfigurationMiscCharge(function (isError, token) {
@@ -17,39 +16,44 @@ var checkEntitlementsFull = function () {
                     console.log('Token from Authenticate Account: ' + token);
                     console.log('Account Id from Authenticate Account: ' + accountId);
                     console.log('Starting: Check Account Entitlement');
+                    this.emit(':tell', 'Full token: '+token);
                     this.attributes["sessionToken"] = token;
                     this.attributes["currentAccountId"] = accountId;
-                    return checkEntitlementsShort(token, accountId);
+                    checkEntitlementsShort(token, accountId, callback);
                 } else {
+                    this.emit(':tell', 'Error in auth account.');
                     console.log('Error in Authenticate Account');
                     console.log(token);
                 }
             });
         } else {
+            this.emit(':tell', 'Error in misc charge.');
             console.log('Error in Configuration Misc Charge');
             console.log(token);
         }
     });
 };
 
-var checkEntitlementsShort = function (token, accountId) {
+var checkEntitlementsShort = function (token, accountId, callback) {
     esuiteInterface.CheckAccountEntitlement(token, accountId, function (isError, entitlements) {
         if (!isError) {
+            this.emit(':tell', 'Short token: '+token);
             console.log('Entitlement from Check Account Entitlement: ' + entitlements);
             if (entitlements) {
                 entitlements.forEach(function (element) {
                     if (entitlement.identifier === "pamplemousse") {
                         console.log('found Pamplemousse');
-                        return true;
+                        callback(false, true);
                     }
                 }, this);
                 console.log('There wasnt any entitlement called Pamplemousse, but there are ' + entitlements.length + ' entitlemnts');
-                return false;
+                callback(true);
             } else {
                 console.log('This user has no entitlements');
-                return false;
+                callback(true);
             }
         } else {
+            this.emit(':tell', 'Error in check short.');
             console.log('Error in Check Account Entitlement');
             console.log(entitlements);
         }
@@ -57,14 +61,14 @@ var checkEntitlementsShort = function (token, accountId) {
 }
 
 var processPayment = function () {
-    var esuiteInterface = new EsuiteInterface();
-    esuiteInterface.processPayment(this.attributes["sessionToken"], function (isError, response) {
-        if (!isError) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+    // var esuiteInterface = new EsuiteInterface();
+    // esuiteInterface.processPayment(this.attributes["sessionToken"], function (isError, response) {
+    //     if (!isError) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // });
 }
 
 exports.handler = function (event, context, callback) {
@@ -90,35 +94,51 @@ var handlers = {
     },
     'GetContentIntent': function () {
         //Checks for entitlements and purchases if needed.
-        var hasEntitlement = false;
-        if(!this.attributes["sessionToken"]){
-            hasEntitlement = checkEntitlementsFull();
-        }else{
-            hasEntitlement = checkEntitlementsShort();
+        // this.emit(':tell', 'lol');
+        // checkEntitlementsFull(function(isError, hasEntitlement){
+        //     if (!isError) {
+        //         if (hasEntitlement) {
+        //             this.emit(':tell', 'Reading full story.');
+        //         } else {
+        //             this.emit(':ask', 'You do not have access to this content. Would you like to make a single purchase; or purchase a subscription?', 'What would you like to do?');
+        //         }
+        //     }
+        //     else{
+        //         this.emit(':tell', 'Error in content check.');
+        //     }
+        // });
+
+        // Configure the request
+        var options = {
+            host: 'https://uat.mppglobal.com',
+            path: '/api/workflows/configurations/adhoc',
+            method: 'POST',
+            'Content-Type': 'application/json'
+        };
+
+        var callback = function(response){
+            this.emit(':tell', 'response is: ' + response);
         }
 
-        this.emit(':tell', 'So far so good.');
-
-        if (hasEntitlement) {
-            this.emit(':tell', 'Reading full story.');
-        } else {
-            this.emit(':ask', 'You do not have access to this content. Would you like to make a single purchase; or purchase a subscription?', 'What would you like to do?');
-        }
+        var req = http.request(options, callback);
+        var postData = '{"orderItems": [{"description": "Grapegruit purchase","priceBreakDown": {"grossAmount": 10}}],"workFlowConfiguration": {"pricing": {"paymentMethod": "CreditCard","currency": "GBP"},"returnUrl": "https://google.com"}}';
+        req.write(postData);
+        req.end();
     },
     'SinglePurchaseIntent': function () {
         //Do process payment.
-        this.attributes["purchaseType"] = "singlePurchase";
+        // this.attributes["purchaseType"] = "singlePurchase";
 
-        var payment = processPayment();
+        this.emit(':tell', ''+this.attributes["sessionToken"]);
 
-        if(payment)
-        {
-            this.emit('GetContentIntent');
-        }
-        else
-        {
-            this.emit(':ask', 'Payment failed; Would you like to try again?', 'Would you like to try again?');
-        }
+        // if(processPayment())
+        // {
+        //     this.emit('GetContentIntent');
+        // }
+        // else
+        // {
+        //     this.emit(':ask', 'Payment failed; Would you like to try again?', 'Would you like to try again?');
+        // }
     },
     'SubscriptionPurchaseIntent': function () {
         //Do add Subscription.
