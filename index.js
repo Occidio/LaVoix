d99964b9caed5822db69ee47e46a46011a55ad79
galpe <Guillaume.Alpe@mppglobal.com>
2016-12-15 12:16:50 +0000

    add Entitolement check api call

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
+'use strict';
 var EsuiteInterface = require('./esuiteInterface');
 var Alexa = require('alexa-sdk');
 
-exports.handler = function(event, context, callback){
+
+var esuiteInterface = new EsuiteInterface();
+console.log('Starting: Config Miss Charge');
+esuiteInterface.ConfigurationMiscCharge(function (isError, token) {
+    if (!isError) {
+        console.log('Token from Config Miss Charge: '+token);
+        console.log('Starting: Authenticate Account');
+        esuiteInterface.AuthenticateAccount(token, function (isError, token, accountId) {
+            if (!isError) {
+                console.log('Token from Authenticate Account: '+token);
+                console.log('Account Id from Authenticate Account: '+accountId);
+                console.log('Starting: Check Account Entitolement');
+                esuiteInterface.CheckAccountEntitolement(token, accountId, function (isError, entitlements) {
+                    if (!isError) {
+                        console.log('Entitolement from Check Account Entitolement: '+entitlements);
+                    }else{
+                        console.log('Error in Check Account Entitolement');
+                        console.log(entitlements);
+                    }
+                });
+            }else{
+                console.log('Error in Authenticate Account');
+                console.log(token);
+            }
+        });
+    }else{
+        console.log('Error in Configuration Misc Charge');
+        console.log(token);
+    }
+});
+
+
+exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = "amzn1.ask.skill.3fcdbcd8-7d87-4def-8542-7ea11b85acdc";
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var PurchaseNews = function(){  
-        this.ConfigurationMiscCharge(function(isError, token){
-            this.emit(':tell','Call to misc done.');
-            if(!isError){           
-                this.AuthenticateAccount(function(isError, token){
-                this.emit(':tell','Call to authenticate done.');
-                    if(!isError){                  
-                        this.emit(':tell','DONE THE THING.');
-                    }
-                });
-            }
-        });
+var PurchaseNews = function () {
+    this.ConfigurationMiscCharge(function (isError, token) {
+        this.emit(':tell', 'Call to misc done.');
+        if (!isError) {
+            this.AuthenticateAccount(function (isError, token) {
+                this.emit(':tell', 'Call to authenticate done.');
+                if (!isError) {
+                    this.emit(':tell', 'DONE THE THING.');
+                }
+            });
+        }
+    });
 }
 
 var instructions = "I can read you the headlines, or get you service fuck.";
@@ -28,13 +61,13 @@ var headline = "Headline one; Do you care?";
 var headlineReprompt = "Would you like to hear the whole story?";
 
 var handlers = {
-    'LaunchRequest': function(){
+    'LaunchRequest': function () {
         //Gives instruction on how to use app.
-        this.emit(':tell',instructions);
+        this.emit(':tell', instructions);
     },
     'GetHeadlinesIntent': function () {
         //Read headlines one by one.
-        this.emit(':ask',headline, headlineReprompt);
+        this.emit(':ask', headline, headlineReprompt);
     },
     'GetContentIntent': function () {
         //Checks for entitlements and purchases is needed.
