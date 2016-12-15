53394ebf452725ca1a892a91fb306728403e7996
galpe <Guillaume.Alpe@mppglobal.com>
2016-12-15 10:18:00 +0000

    test alexa packege

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 var EsuiteInterface = require('./esuiteInterface');
-var http = require('http');
+// var http = require('http');
+var Alexa = require('alexa-sdk');
 
-var authCallback = function(isError, response){
-    if(isError){
-			console.log('ERROR:' + response);
-  }else{
-      console.log('authCallback Token:' + response);
-  }
-}
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
 
-var miscCallback = function(isError, response){
-  if(isError){
-			console.log('ERROR:' + response);
-  }else{
-      console.log('miscCallback Token:' + response);
+var handlers = {
+    'HelloWorldIntent': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
+
+// var authCallback = function(isError, response){
+//     if(isError){
+// 			console.log('ERROR:' + response);
+//   }else{
+//       console.log('authCallback Token:' + response);
+//   }
+// }
+
+// var miscCallback = function(isError, response){
+//   if(isError){
+// 			console.log('ERROR:' + response);
+//   }else{
+//       console.log('miscCallback Token:' + response);
       
-      var esuiteInterface = new EsuiteInterface();
-      esuiteInterface.AuthenticateAccount(response, authCallback);
-  }
-}
+//       var esuiteInterface = new EsuiteInterface();
+//       esuiteInterface.AuthenticateAccount(response, authCallback);
+//   }
+// }
 
-var server = http.createServer(function(req, res) {
-  var esuiteInterface = new EsuiteInterface();
-  res.writeHead(200);
-  esuiteInterface.ConfigurationMiscCharge(miscCallback);
-});
-server.listen(8080);
+// var server = http.createServer(function(req, res) {
+//   var esuiteInterface = new EsuiteInterface();
+//   res.writeHead(200);
+//   esuiteInterface.ConfigurationMiscCharge(miscCallback);
+// });
+// server.listen(8080);
