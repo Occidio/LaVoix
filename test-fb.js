99161ad1b083b9a3bcc7d2217151a6ea062dda4a
galpe <Guillaume.Alpe@mppglobal.com>
2016-12-19 09:29:19 +0000

    Post to Facebook

--- test-fb.js
+++ test-fb.js
@@ -1,11 +1,16 @@
 var https = require('https');
+var FB_APP_ID = '1218757314803008';
+var FB_APP_SECRET = '87b220f191da2b0589c7c001398dcb8f';
+var FB_USER_ID = '100012931541050';
+var FB_ACCESS_TOKEN_POST_ACTION = 'EAARUdAzQ9UABAKMN78cVUFjn1rIbQIlJrIWiO5ma2OZC4Kk3C50rDzoGgb6byef7PZCJG2aBwzz6U4wcpfA26vDR8cL21vd2DHvZBCZBhZCCbVqsYSQrRGQI91ZCwIY6EgYxZCJ2lXJGR0hNAnnGwZA3eNOQlOqYa14gu97k7UC4GMtN2V5i25nM';
 
-var callFB = function(access_token) {
+
+var callFB = function (access_token) {
     // An object of options to indicate where to post to
     var post_options = {
         host: 'graph.facebook.com',
         port: '443',
-        path: '/511214920?access_token='+access_token, //FB Id https://lookup-id.com/#
+        path: '/' + FB_USER_ID + '?access_token=' + access_token, //FB Id https://lookup-id.com/#
         method: 'GET'
     };
 
@@ -32,12 +37,12 @@ var callFB = function(access_token) {
     post_req.end();
 };
 
-var FB_oauth = function() {
+var FB_oauth = function () {
     // An object of options to indicate where to post to
     var post_options = {
         host: 'graph.facebook.com',
         port: '443',
-        path: '/oauth/access_token?client_id=193234604474088&client_secret=294e7ac18f703c486285a2452ce7394c&grant_type=client_credentials', //FB Id https://lookup-id.com/#
+        path: '/oauth/access_token?client_id=' + FB_APP_ID + '&client_secret=' + FB_APP_SECRET + '&grant_type=client_credentials', //FB Id https://lookup-id.com/#
         method: 'GET'
     };
 
@@ -53,9 +58,73 @@ var FB_oauth = function() {
         response.on('end', function () {
             //ConfigAdhocSuccess(JSON.parse(str).sessionToken);
             console.log(str);
-            console.log(str).substrig(29);
+            var accessToken = str.substring(30);
+            console.log(accessToken);
             //var access_token = //access_token=193234604474088|5zv5-e1o0Bi9EAif6ESeiOM9AH4
-            callFB();
+            callFB(accessToken);
+        });
+    };
+
+    // Set up the request
+    var post_req = https.request(post_options, callback);
+
+    // post the data
+    //post_req.write(post_data);
+    post_req.end();
+};
+
+var FB_postToFeed = function (userId, access_token, message) {
+    // An object of options to indicate where to post to
+    var post_options = {
+        host: 'graph.facebook.com',
+        port: '443',
+        path: '/' + userId + '/feed?access_token=' + access_token, //FB Id https://lookup-id.com/#
+        method: 'POST'
+    };
+
+    var post_data = '{"message": ' + message + '}';
+
+    callback = function (response) {
+        var str = '';
+
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            console.log(str);
+        });
+    };
+
+    // Set up the request
+    var post_req = https.request(post_options, callback);
+
+    // post the data
+    post_req.write(post_data);
+    post_req.end();
+};
+
+var FB_postToFeed2 = function (userId, access_token, message) {
+    // An object of options to indicate where to post to
+    var post_options = {
+        host: 'graph.facebook.com',
+        port: '443',
+        path: '/me/feed', //FB Id https://lookup-id.com/#
+        method: 'POST',
+        query: 'mesage=' + message + '&access_token=' + access_token
+    };
+
+    //var post_data = '{"message": '+message+'}';
+
+    callback = function (response) {
+        var str = '';
+
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            console.log(str);
         });
     };
 
@@ -68,6 +137,52 @@ var FB_oauth = function() {
 };
 
 
-callFB();
-FB_oauth();
+var testFromPostMan = function () {
+    var testMessage = 'TestMessage' + timestamp();
+    var options = {
+        "method": "POST",
+        "hostname": "graph.facebook.com",
+        "port": null,
+        "path": "/me/feed?message=" + testMessage + "&access_token=EAARUdAzQ9UABAKWXIx0MA9eiXH0l4ynBmucc4MQYuitfR0s13BY6ioiUHJiszyjMW5t7S1o01JZBr4sdZA9gngT5QGqqdvt8XWOPD9U0dqVv7Q97J6NVnRCua8POeQfPypwa8TPvrx4wEZAENpGdRrZBgKf9O2Q77ckky63nfJ1EE1QwXoQY",
+        "headers": {
+            "cache-control": "no-cache"
+        }
+    };
 
+    console.log(options);
+    var req = https.request(options, function (res) {
+        var chunks = [];
+
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
+    });
+
+    req.end();
+};
+
+var timestamp = function () {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var yyyy = today.getFullYear();
+        var hh = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+
+        return '' + yyyy + mm + dd + '_' + hh + m + s;
+    }
+    //  /{user-id}/feed
+
+
+// var userToken = 'EAARUdAzQ9UABAEQHkzYFYxtNYk0nyTBnB9zUVtN6cbeqcCcXZCU1SMiOpFca1fLCeJn9hJuGnZCqT9X0NV9kj0JILEeZAZAZCwTIN74IHIEeOvZBIGPKqZA33BU2JCSig4x4A6A7ZC5MaCiQXmVQx6dLjZB6ozOfq6tXmj008cjgSRmE8XycPg8nI';
+// callFB(userToken);
+
+//FB_postToFeed2(FB_USER_ID, FB_ACCESS_TOKEN_POST_ACTION, "TEST POST TO FB FROM NODE");
+//FB_oauth();
+testFromPostMan();
